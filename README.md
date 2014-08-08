a863f8df7309e9a965aafe8631e1d0677dc023aa
Franklin Chen <franklinchen@franklinchen.com>
2014-08-07 23:21:49 -0400

    Initial

---
+++ README.md
@@ -0,0 +1,13 @@
+## Clojure transducers in a typed setting, in Haskell
+
+I was struggling to understand Rich Hickey's notion of *transducers* in Clojure that he announced in a [blog post](http://blog.cognitect.com/blog/2014/8/6/transducers-are-coming) and posted some [sample code](https://gist.github.com/richhickey/b5aefa622180681e1c81) for in response to a [discussion thread](https://news.ycombinator.com/item?id=8143905). The discussion there was very confusing to me, and I decided I could not really understand what was going on until I actually reimplemented the ideas in a statically typed language.
+
+Fortunately, on another [discussion thread](http://www.reddit.com/r/haskell/comments/2cv6l4/clojures_transducers_are_perverse_lenses/), he posted actually running [Haskell code](http://www.reddit.com/r/haskell/comments/2cv6l4/clojures_transducers_are_perverse_lenses/cjjyay7), which helped me **tremendously**, since most of the discussion I had seen so far was either very vague or very abstract.
+
+So I refactored his code to fit just exactly what he did, not something far more general, and posted my refactored code in response. I hope this will help more people understand *exactly* what they do, using types.
+
+### Rank-2 types
+
+Note that the critical component of this work involves using [rank-2 types](http://www.haskell.org/haskellwiki/Rank-N_types). Transducers *cannot* be expressed in a weaker type system that does not support higher-rank types.
+
+I hope that the introduction of transducers will create a lot of interest among those who are not already familiar with higher-rank types.
---
+++ Transducers.hs
@@ -0,0 +1,89 @@
+{-
+  Clojure transducers translated into Haskell.
+
+  http://blog.cognitect.com/blog/2014/8/6/transducers-are-coming
+
+  Based on Rich Hickey's preliminary Haskell version at
+  http://www.reddit.com/r/haskell/comments/2cv6l4/clojures_transducers_are_perverse_lenses/
+-}
+
+{-# LANGUAGE Rank2Types #-}
+
+-- For example using Vector instead of list
+import qualified Data.Vector as V
+
+-- Left reduce
+type Reducer a r = r -> a -> r
+
+-- Here's where then rank-2 type is needed
+type Transducer a b = forall r . Reducer a r -> Reducer b r
+
+-- Left fold
+class Foldable t where
+  fold :: Reducer a r -> r -> t a -> r
+
+class Conjable f where
+  empty :: f a
+  conj :: Reducer a (f a)
+
+mapping :: (b -> a) -> Transducer a b
+mapping f xf r a = xf r (f a)
+
+filtering :: (a -> Bool) -> Transducer a a
+filtering p xf r a = if p a then xf r a else r
+
+flatmapping :: Foldable f => (a -> f b) -> Transducer b a
+flatmapping f xf r a = fold xf r (f a)
+
+-- I changed Rich Hickey's code to be more general than just list
+-- but accept anything Conjable
+xlist :: (Foldable f, Conjable f) => Transducer a b -> f b -> f a
+xlist xf = fold (xf conj) empty
+
+-- build any old Foldable function with its transducer, all the same way
+xmap :: (Foldable f, Conjable f) => (a -> b) -> f a -> f b
+xmap f = xlist $ mapping f 
+
+xfilter :: (Foldable f, Conjable f) => (a -> Bool) -> f a -> f a
+xfilter p = xlist $ filtering p
+
+xflatmap :: (Foldable f, Conjable f) => (a -> f b) -> f a -> f b
+xflatmap f = xlist $ flatmapping f
+
+-- Stuff specialized to lists.
+-- To use another type, just make it a Foldable and Conjable.
+instance Foldable [] where
+  fold = foldl
+
+-- for exposition only, yes, conj is gross for lazy lists
+-- in Clojure conj and left folds dominate
+instance Conjable [] where
+  empty = []
+  conj xs x = xs ++ [x]
+
+-- Note: the type does not say anything about Foldable or Conjable,
+-- even though the implementation just happens to use a list!
+xform :: Transducer Integer Integer
+xform = mapping (+ 1) . filtering even . flatmapping (\x -> [0 .. x])
+
+-- Again, this can munge anything Foldable and Conjable, not just a list.
+munge :: (Foldable f, Conjable f) => f Integer -> f Integer
+munge = xlist xform
+
+-- munge a list
+-- [0,1,2,0,1,2,3,4,0,1,2,3,4,5,6]
+example1 :: [Integer]
+example1 = munge [1..5]
+
+-- Implement Foldable, Conjable type classes for Vector
+instance Foldable V.Vector where
+  fold = V.foldl
+
+instance Conjable V.Vector where
+  empty = V.empty
+  conj = V.snoc
+
+-- return a vector rather than a list; note the fact that munge actually
+-- internally uses a list
+example2 :: V.Vector Integer
+example2 = munge $ V.enumFromN 1 5
